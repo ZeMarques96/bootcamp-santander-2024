793c0378328700d53d11f1a5c06060fe97f2635d
José Francisco Marques Neto <netomarques96@gmail.com>
2024-06-20 23:49:12 -0300

    Inicializando o projeto

---
+++ src/main/java/me/dio/bootcamp_santander_2024/Application.java
@@ -0,0 +1,13 @@
+package me.dio.bootcamp_santander_2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampSantander2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampSantander2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/bootcamp_santander_2024/BootcampSantander2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.bootcamp_santander_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampSantander2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
